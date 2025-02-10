53237b11f26c2a04e0ca984373a93dfe5eae159f
matildacr <al03096121@tecmilenio.mx>
2025-02-09 18:15:00 -0600

    m

--- db.js
+++ db.js
@@ -2,17 +2,20 @@ const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
     host: 'mydevmysql2025.mysql.database.azure.com',
-    user: 'fullstack',  // Cambia esto según tu usuario de MySQL
-    password: 'Mamberroi0725.',  // Pon tu contraseña de MySQL si tienes una
-    database: 'CALCULADORA'
+    user: 'fullstack',
+    password: 'Mamberroi0725.',
+    database: 'CALCULADORA',
+    ssl: {
+        rejectUnauthorized: true
+    }
 });
 
 connection.connect(err => {
     if (err) {
-        console.error('Error conectando a la BD:', err);
+        console.error('❌ Error conectando a la BD:', err);
         return;
     }
-    console.log('Conexión a MySQL exitosa.');
+    console.log('✅ Conexión a MySQL en Azure exitosa.');
 });
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const db = require('./db');
 
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 const SECRET_KEY = 'secreto_super_seguro';
 
 app.use(bodyParser.json());
