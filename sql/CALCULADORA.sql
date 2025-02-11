79ddfc70bf781a316ead15bfffb26dd72e785df2
matildacr <al03096121@tecmilenio.mx>
2025-02-11 12:50:10 -0600

    m

--- sql/CALCULADORA.sql
+++ sql/CALCULADORA.sql
@@ -1,15 +1,8 @@
-create database CALCULADORA;
-USE CALCULADORA;
+CREATE DATABASE fullstackcourse;
+USE fullstackcourse;
 
-CREATE TABLE User (
-		ID_user int auto_increment not null,
-        username varchar (100) not null,
-        password varchar (100) not null,
-        PRIMARY KEY (ID_user)
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
 );
-
-create table user_session(
-	session_id varchar (100) not null,
-	ID_user int not null,
-    CONSTRAINT fk_id_user FOREIGN KEY (ID_user) REFERENCES User(ID_user)
-)
