808658329faaaa3d835d393aa7e9d8e1999920a2
DavidSilva1376 <al02958539@tecmilenio.mx>
2025-02-19 12:33:47 -0600

    mejora en la conexion de la base de datos

--- server.js
+++ server.js
@@ -87,5 +87,6 @@ app.post('/auth/logout', (req, res) => {
     });
 });
 
-// Iniciar el servidor
-app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
+});
