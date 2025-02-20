fd7a626e36d2122c8385f190bc963eaa2a4910f6
Adrian-G <AL02996024@tecmilenio.mx>
2025-02-20 11:38:07 -0600

    cambios al diseño de boton de modo oscuro

--- public/scriptLightDark.js
+++ public/scriptLightDark.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("imgLightDark").addEventListener("click", function () {
         const colorScheme = document.body.style.colorScheme;
+        this.classList.toggle("inverted");//Llama al estilo de colores invertido para el boton
         document.body.style.colorScheme = (colorScheme === "dark") ? "light" : "dark";
     })
     const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
     document.body.style.colorScheme = (isDark) ? "dark" : "light";
 });
+
