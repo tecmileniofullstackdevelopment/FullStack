44f0796d492743958534aa590f8a1921fd9fc96c
alumnocuenta <196365244+alumnocuenta@users.noreply.github.com>
2025-02-19 18:26:11 -0600

    shorten if else

--- public/scriptLightDark.js
+++ public/scriptLightDark.js
@@ -1,11 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("imgLightDark").addEventListener("click", function () {
         const colorScheme = document.body.style.colorScheme;
-        if (colorScheme === "dark") {
-            document.body.style.colorScheme = "light";
-        } else {
-            document.body.style.colorScheme = "dark";
-        }
+        document.body.style.colorScheme = (colorScheme === "dark") ? "light" : "dark";
     })
     const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
     document.body.style.colorScheme = (isDark) ? "dark" : "light";
