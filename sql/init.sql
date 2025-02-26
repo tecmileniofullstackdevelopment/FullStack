64b99e421b58a3d59b272de1be41a081a01c2e25
AndresVallejo888 <al02970656@tecmilenio.mx>
2025-02-25 21:25:21 -0600

    Correcion codigo sql
    Se cambio de int a varchar el session id para que no marcara error al intentar guardarlo en la tabla

--- sql/init.sql
+++ sql/init.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE bddCalculadora;
+-- CREATE DATABASE bddCalculadora;
 USE bddCalculadora;
 
 CREATE TABLE users (
@@ -6,10 +6,12 @@ CREATE TABLE users (
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
 );
-
 create table user_session (
-	user_id int,
-    session_id int auto_increment primary key,
-    created_at datetime,
+	user_id int not null,
+    session_id varchar(60) primary key,
+    created_at datetime not null,
     CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+select * from users;
+select * from user_session;
