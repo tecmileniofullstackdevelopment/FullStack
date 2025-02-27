4676c8d774fd716c830dc602385fc29d3d970700
Adrian-G <AL02996024@tecmilenio.mx>
2025-02-27 14:57:17 -0600

    correcciones de color de barra de carga
    la barra y su fondo cambian según el modo claro y oscuro, para cambiar el GIF se le llama en JavaScript y se aplica el filtro invertido

--- public/scriptLightDark.js
+++ public/scriptLightDark.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("imgLightDark").addEventListener("click", function () {
         const colorScheme = document.body.style.colorScheme;
+        const progBarGif = document.getElementById("walk");
         this.classList.toggle("inverted");//Llama al estilo de colores invertido para el boton
+        progBarGif.classList.toggle("inverted"); //Llama al estilo de colores invertido para el gif
         document.body.style.colorScheme = (colorScheme === "dark") ? "light" : "dark";
     })
     const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
