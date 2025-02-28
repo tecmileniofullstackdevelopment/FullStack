7b828393ee801a7d17543d0de442f54a345b02dd
Adrian-G <AL02996024@tecmilenio.mx>
2025-02-28 12:21:00 -0600

    Modo claro y oscuro guardado en meomoria de la pagina
    El script guarda la preferencia y la aplica en las demás páginas (no en la calculadora)

--- public/scriptLightDark.js
+++ public/scriptLightDark.js
@@ -1,12 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
+    const toggleButton = document.getElementById("imgLightDark");
+    const progBarGif = document.getElementById("walk");
+
+    const savedTheme = localStorage.getItem("theme");
+
+    if (savedTheme) {
+        document.body.style.colorScheme = savedTheme; 
+    } else {
+        const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+        document.body.style.colorScheme = isDark ? "dark" : "light";
+    }
+
+    if (document.body.style.colorScheme === "dark") {
+        toggleButton.classList.add("inverted");
+        if(progBarGif !== null){
+            progBarGif.classList.add("inverted");
+        } 
+    } else {
+        toggleButton.classList.remove("inverted");
+        if(progBarGif !== null){
+            progBarGif.classList.remove("inverted");
+        }
+    }
+
     document.getElementById("imgLightDark").addEventListener("click", function () {
         const colorScheme = document.body.style.colorScheme;
-        const progBarGif = document.getElementById("walk");
+        const newTheme = document.body.style.colorScheme = (colorScheme === "dark") ? "light" : "dark";
+        document.body.style.colorScheme = newTheme;
+        localStorage.setItem("theme", newTheme);
         this.classList.toggle("inverted");//Llama al estilo de colores invertido para el boton
-        document.body.style.colorScheme = (colorScheme === "dark") ? "light" : "dark";
         progBarGif.classList.toggle("inverted"); //Llama al estilo de colores invertido para el gif
-    })
-    const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
-    document.body.style.colorScheme = (isDark) ? "dark" : "light";
+    });
 });
 
