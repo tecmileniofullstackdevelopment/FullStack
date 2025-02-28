44b8a096f8e9810e6dda4fa822f8951c13114183
Adrian-G <AL02996024@tecmilenio.mx>
2025-02-28 11:00:03 -0600

    cambio de diseño para borrar historial de calculadora

--- public/scriptLightDark.js
+++ public/scriptLightDark.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
         const colorScheme = document.body.style.colorScheme;
         const progBarGif = document.getElementById("walk");
         this.classList.toggle("inverted");//Llama al estilo de colores invertido para el boton
-        progBarGif.classList.toggle("inverted"); //Llama al estilo de colores invertido para el gif
         document.body.style.colorScheme = (colorScheme === "dark") ? "light" : "dark";
+        progBarGif.classList.toggle("inverted"); //Llama al estilo de colores invertido para el gif
     })
     const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches
     document.body.style.colorScheme = (isDark) ? "dark" : "light";
