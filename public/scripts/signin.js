852e4f83ba1f77d73165c0a15824c987d2c541ff
AndresTirado-stack <AL02958380@tecmilenio.mx>
2025-03-03 10:43:40 -0600

    Update signin.js

--- public/scripts/signin.js
+++ public/scripts/signin.js
@@ -2,9 +2,9 @@ document.getElementById("togglePassword").addEventListener("click", function() {
     let passwordInput = document.getElementById("password");
     if (passwordInput.type === "password") {
         passwordInput.type = "text";
-        this.textContent = "Hide password";
-    } else{
+        this.textContent = "Conceal password";
+    } else {
         passwordInput.type = "password";
-        this.textContent = "Show password";
+        this.textContent = "Provide password";
     }
 });
