7bf545555ef351c76d6b400b1f9bbd3295965626
Hector Leal <136043873+no-c-123@users.noreply.github.com>
2025-03-03 14:36:26 -0600

    coneccion a base de datos

--- init.sql
+++ init.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE fullstack_login;
 USE fullstack_login;
 
-CREATE TABLE users (
+CREATE TABLE user (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL
